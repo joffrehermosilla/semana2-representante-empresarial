f6f9eabd82c1b98c6fd639cec5e836abf56543b1
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 11:55:50 -0500

    primer commit

---
+++ microservicio-representante-empresarial/src/main/java/nttdata/bootcamp/microservicios/representante/empresarial/MicroservicioRepresentanteEmpresarialApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.representante.empresarial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioRepresentanteEmpresarialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioRepresentanteEmpresarialApplication.class, args);
+	}
+
+}
